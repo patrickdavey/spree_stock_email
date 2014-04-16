501ffc89721746aa93e250a6dd2d4cd1ee9ac812
Patrick Davey <patrick.davey@resolvedigital.co.nz>
2014-04-17 09:53:00 +1200

    Allow signup over JSON

--- app/controllers/spree/stock_emails_controller.rb
+++ app/controllers/spree/stock_emails_controller.rb
@@ -1,18 +1,15 @@
 class Spree::StockEmailsController < ApplicationController
-
   def create
     variant = Spree::Variant.find_by(id: stock_email_params[:variant])
-    redirect_to :back and return unless variant
+    render :json => { errors: 'No variant found' }, status: :unprocessable_entity and return unless variant
 
     current_email = spree_current_user ? spree_current_user.email : stock_email_params[:email]
     stock_email = Spree::StockEmail.new(variant: variant, email: current_email)
     if stock_email.save
-      flash[:success] = Spree.t('stock_email.messages.back_in_stock_email_message', product: variant.product.name)
+      render json: { message: Spree.t('stock_email.messages.back_in_stock_email_message', product: variant.product.name) }
     else
-      flash[:notice] = stock_email.errors.full_messages.join(', ')
+      render json: { errors: stock_email.errors.full_messages }, status: :unprocessable_entity
     end
-
-    redirect_to :back
   end
 
   private
